58c99b4721617370624df57893186420ea7cd2dc
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-08-31 17:58:20 +0530

    started

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <>
+    <h2>Expense Item</h2>
+    <p>Food Rs 10</p>
+    <p>Petrol Rs 100</p>
+    <p>Movies Rs 200</p>
+    </>
+  )
+}
