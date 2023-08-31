9e191c595336e304233952b512d7bb36fc182b8c
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-08-31 18:20:02 +0530

    location

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,12 +6,14 @@ export default function ExpenseItem() {
 const expenseDate= new Date(2021, 2, 28);
 const expenseTitle = 'Car Insurance';
 const expenseAmount = 294.67
+const locationofExpenditure = "Showroom"
 
   return (
     <div className='expense-item'>
 <div>{expenseDate.toISOString()}</div>
 <div className='expense-item__description'>
     <h2>{expenseTitle}</h2>
+    <h2>{locationofExpenditure}</h2>
     <div className='expense-item__price'>${expenseAmount}</div>
 </div>
     </div>
