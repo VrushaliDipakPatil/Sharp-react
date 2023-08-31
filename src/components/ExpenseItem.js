d3487b7e09d48cea38ac3f6b943d31b64c9151c8
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-08-31 18:16:44 +0530

    dynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './expenseitem.css';
 
 export default function ExpenseItem() {
+
+const expenseDate= new Date(2021, 2, 28);
+const expenseTitle = 'Car Insurance';
+const expenseAmount = 294.67
+
   return (
-    <>
-    <h2>Expense Item</h2>
-    <p>Food Rs 10</p>
-    <p>Petrol Rs 100</p>
-    <p>Movies Rs 200</p>
-    </>
+    <div className='expense-item'>
+<div>{expenseDate.toISOString()}</div>
+<div className='expense-item__description'>
+    <h2>{expenseTitle}</h2>
+    <div className='expense-item__price'>${expenseAmount}</div>
+</div>
+    </div>
   )
 }
