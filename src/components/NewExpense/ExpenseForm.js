25ac25338a534d59222e8491670152a2db05bed6
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-01 18:30:55 +0530

    form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,18 +5,10 @@ const ExpenseForm = (props) => {
   const [enteredtitle, setEnteredTitle] = useState("");
   const [enteredamount, setEnteredAmount] = useState("");
   const [entereddate, setEnteredDate] = useState("");
-
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [isEditing, setIsEditing] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
   };
 
   const amountChangeHandler = (event) => {
@@ -27,52 +19,76 @@ const ExpenseForm = (props) => {
     setEnteredDate(event.target.value);
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
-    const expenseData={
-        title: enteredtitle,
-        amount: enteredamount,
-        date: new Date(entereddate)
-    }
+    const expenseData = {
+      title: enteredtitle,
+      amount: enteredamount,
+      date: new Date(entereddate),
+    };
 
     props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('')
-    setEnteredDate('')
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    stopEditingHandler(); // Close the form after adding an expense
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={enteredtitle} onChange={titleChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredamount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={entereddate}
-            onChange={dateChangeHandler}
-          />
-        </div>
+    <div>
+      {isEditing ? (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredtitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredamount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={entereddate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button" onClick={stopEditingHandler}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      ) : (
+        <div className="centered">
+        <button onClick={startEditingHandler}>Add Expense</button>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      )}
+    </div>
   );
 };
 
