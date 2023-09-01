40c3b9d556df1844a64ffe7d5b8ba10cdca4c3f0
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-01 16:05:11 +0530

    filter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(props) {
+
+  const[filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear =>{
+    setFilteredYear(selectedYear)
+}
+
   return (
     <div className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}  />
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
