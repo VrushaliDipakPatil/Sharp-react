face2124816d79cd2cb2efe97b7458c36f1521f5
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-04 14:06:04 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import { useEffect, useState } from "react";
-import FoemInput from "./components/FoemInput";
-import Orders from "./components/Orders";
-
-
+import Header from "./components/Layout/Header";
 
 function App() {
-
-  const [orders, setOrders] = useState([]);
-
-  useEffect(() => {
-    const savedOrders = JSON.parse(localStorage.getItem("orders")) || [];
-    setOrders(savedOrders);
-  }, []);
-
-  const handleAddToBill = (orderData) => {
-    const updatedOrders = [...orders, orderData];
-    setOrders(updatedOrders);
-    localStorage.setItem("orders", JSON.stringify(updatedOrders));
-  };
-
-  const handleUpdateOrders = (updatedOrders) => {
-
-    setOrders(updatedOrders);
-    localStorage.setItem("orders", JSON.stringify(updatedOrders));
-  };
-
   return (
-    <div>
-<FoemInput onAddToBill={handleAddToBill}/>
-<Orders orders={orders} updateOrdersInLocalStorage={handleUpdateOrders}/>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
 export default App;
-
-
-
-
-
-
