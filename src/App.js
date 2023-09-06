0c1d9ed9c4ed09c72bf50dc9333153d5d45917ca
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-06 11:16:12 +0530

    restaurant app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
 
-import './App.css';
-import Button from './components/UI/Button/Button';
-import DemoList from './components/Demo/DemoList';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
-  const [isAscending, setIsAscending] = useState(true);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
-  }, []);
-
-  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
-
-  const sortedList = () => {
-    return isAscending ? listItems.sort((a,b)=> a-b) : listItems.sort((a,b)=> b-a);
+  const showCartHandler = () => {
+    setCartIsShown(true);
   };
 
-  const toggleSortingHandler = useCallback(() => {
-    setIsAscending((prevState) => !prevState);
-  }, []);
-
-  const buttonText = isAscending ? 'Change to Descending Order' : 'Change to Ascending Order';
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
-    <div className="app">
-      <DemoList title={listTitle} items={sortedList} />
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
-      <Button onClick={toggleSortingHandler}>{buttonText}</Button>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
