15db63794b8056ad54540a6d443d9ca07670ac04
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-11 22:09:27 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Card from './Component/Card';
-import Footer from './Component/Footer';
-import Navbar from './Component/Navbar';
-import Home from './Component/Home';
-import About from './Component/About';
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Fetching movies failed");
+      }
+
+      const data = await response.json();
+
+      const transformedData = data.results.map((data) => {
+        return {
+          id: data.episode_id,
+          releaseDate: data.release_date,
+          openingText: data.opening_crawl,
+        };
+      });
+
+      setMovies(transformedData);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  }
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-          <Route path="/home" exact element={<Home/>} />
-          <Route path="/" element={<Card/>} />
-          <Route path="/about" element={<About/>} />
-          </Routes>
-        <Footer />
- 
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JavaScript
-import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './Component/CartContext';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <CartProvider>
-    <App />
-    </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
