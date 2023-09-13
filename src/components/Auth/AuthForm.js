6fb5f039d4c930b15e25d0cca1f30c241bf22349
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-13 12:02:58 +0530

    login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,27 +1,66 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const[isloading, setIsLoading] = useState(false);
+  const [isLoding, setIsLoding] = useState(false);
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const handleChange = (event) =>{
-    const {name, value} = event.target;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setUser({
-...user,
-      [name]: value
-    })
-  }
+      ...user,
+      [name]: value,
+    });
+  };
 
-  const handleSubmit = (event) =>{
-event.preventDefault();
-setIsLoading(true)
-  }
+  const SubmitHandler = (event) => {
+    event.preventDefault();
+    setIsLoding(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7XEOc6O8Svn_udxhDjDsiXVK2J2lv66c";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7XEOc6O8Svn_udxhDjDsiXVK2J2lv66c";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: user.email,
+        password: user.password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoding(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication Failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -30,23 +69,37 @@ setIsLoading(true)
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={SubmitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" name="email" value={user.email} onChange={handleChange} required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" name="password" value={user.password} onChange={handleChange} required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <div>
-          {isloading? "Sending Request..." : 
-                    <button className={classes.button}>
-                    {isLogin ? "Login" : "Create Account"}
-                  </button> }
-
-        </div>
+        <div></div>
         <div className={classes.actions}>
+          {isLoding ? (
+            "Sending Request...."
+          ) : (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+
           <button
             type="button"
             className={classes.toggle}
