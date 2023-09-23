dfcda83d00f6ad665e7116f3c4dca3810df41918
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-23 15:12:35 +0530

    custom hook

--- src/components/MailCompose.js
+++ src/components/MailCompose.js
@@ -2,68 +2,30 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Import styles
 import { useSelector } from 'react-redux';
+import useSendEmail from '../CustomHooks/useSendEmail';
 
 const MailCompose = () => {
     const [to, setTo] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
-    const senderemail = useSelector((state) => state.auth.user_email)
+    const senderEmail = useSelector((state) => state.auth.user_email)
 
-    const removeSpecialCharacters = (email) => {
-      return email.replace(/[@.]/g, '');
-    };
+    const { sendEmail } = useSendEmail(); // Initialize the custom hook
 
-  
     const handleSendEmail = async () => {
       try {
-        const receiverUserId = removeSpecialCharacters(to); // Replace with actual receiver's user ID
-        const senderUserId = removeSpecialCharacters(senderemail); // Replace with actual sender's user ID
-  
-        // Send the email to the receiver
-        const senderresponse = await fetch(`https://react-mail-c09ee-default-rtdb.firebaseio.com/email/sent/${senderUserId}.json`,{
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            From : senderemail,
-            Subject: subject,
-            Message : message,
-            To: to,
-          })
-        })
-        if(!senderresponse.ok){
-          throw new Error("Mail not sent")
-        }
-  
-        // Store the email in the sender's "sent" folder
-        const receiverresponse = await fetch(`https://react-mail-c09ee-default-rtdb.firebaseio.com/email/inbox/${receiverUserId}.json`,{
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            From : senderemail,
-            Subject: subject,
-            Message : message,
-            To: to,
-            isRead: false
-          })
-        })
-        if(!receiverresponse.ok){
-          throw new Error("Mail not sent")
-        }
-  
+        await sendEmail(senderEmail, to, subject, message);
         alert('Email sent successfully.');
-        setMessage('')
-        setSubject('')
-        setTo('')
+        setMessage('');
+        setSubject('');
+        setTo('');
       } catch (error) {
-        console.error('Failed to send email:', error);
         alert('Failed to send email.');
       }
     };
+
+
     
   return (
     <div className="container" style={{width: '50%' }}>
