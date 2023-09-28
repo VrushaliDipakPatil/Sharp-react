83c7f4496c86cae620268fe3a04b560b8844b396
patil-vrushali <Vrushali.p@tecxlabs.com>
2023-09-28 15:41:08 +0530

    nested route in nextjs

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const developer = () => {
+  return (
+    <div>My Name</div>
+  )
+}
+
+export default developer
---
+++ pages/news/something-important.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const somethingimportant = () => {
+  return (
+    <div>something-important</div>
+  )
+}
+
+export default somethingimportant
